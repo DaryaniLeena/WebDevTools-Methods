30c016823cef6603ff73a8af8129d77e2341f3e5
DaryaniLeena <daryani.l@husky.neu.edu>
2020-09-25 16:53:40 -0700

    assignment2 changes

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -2,9 +2,19 @@
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY
+function compare( word, guess ) { 
+  
+  // DO NOT MODIFY
+  word = word.toLowerCase(); // change words to lowercase to ignore case while
+  guess = guess.toLowerCase();
+  let count = 0;
+  const charArray = guess.split("");
+  for(let letter of word){    let index = charArray.findIndex(char => char === letter);
 
-/* YOU MAY MODIFY THE LINES BELOW */
-
-  return 0; // this line is wrong
+    if(index >= 0){
+      count++;
+      charArray.splice(index, 1);
+    }
+  }
+  return count; // this line is wrong
 }
