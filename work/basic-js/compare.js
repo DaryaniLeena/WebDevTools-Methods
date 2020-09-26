abd92e9eeb07b02a7e4ae0be145847a7a353fd07
DaryaniLeena <daryani.l@husky.neu.edu>
2020-09-26 16:53:38 -0700

    Changed logic for  assingment 2 and updated gitignore

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -4,17 +4,28 @@ module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
 function compare( word, guess ) { 
   
-  // DO NOT MODIFY
-  word = word.toLowerCase(); // change words to lowercase to ignore case while
-  guess = guess.toLowerCase();
-  let count = 0;
-  const charArray = guess.split("");
-  for(let letter of word){    let index = charArray.findIndex(char => char === letter);
+  let count = 0;  // To store the count of valid pairs 
+  let comparison = 'a';   // To find integer position of a char in array
+  let wordArray = new Array(26); 
+  wordArray.fill(0); 
+  let guessArray = new Array(26); 
+	guessArray.fill(0); 
 
-    if(index >= 0){
-      count++;
-      charArray.splice(index, 1);
-    }
+	
+  word = word.toLowerCase();  // Change words to lowercase to ignore case while
+  guess = guess.toLowerCase();
+	
+	for (let i = 0; i < word.length; i++) {   // Update the frequencies of the characters of string s1 
+    wordArray[word.charAt(i).charCodeAt(0) - comparison.charCodeAt(0)]++; 
+}
+	
+	for (let i = 0; i < guess.length; i++) {   // Update the frequencies of the characters of string s2 
+    guessArray[guess.charAt(i).charCodeAt(0) - comparison.charCodeAt(0)]++; 
   }
-  return count; // this line is wrong
+
+	for (let i = 0; i < 26; i++){   // Find the count of valid pairs 
+    count += (Math.min(wordArray[i], guessArray[i])); 
+  }
+
+	return count; 
 }
