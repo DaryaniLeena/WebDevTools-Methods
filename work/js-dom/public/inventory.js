ac0a22b656331e3f6f1f9ab86932f2102afe6d6f
DaryaniLeena <daryani.l@husky.neu.edu>
2020-10-25 09:58:49 -0700

    changes for inventory app

--- work/js-dom/public/inventory.js
+++ work/js-dom/public/inventory.js
@@ -1,117 +1,93 @@
 "use strict";
 (function iife() {
-let items=[{
-    "name":"pasta",
-    "value":2,
-    
-},{
-    "name":"green tea",
-    "value":0,
-}];
 
-const inputItem=document.querySelector('#inventory-app .input-item');
-const addButton=document.querySelector('#inventory-app .add-button');
-const item=document.querySelector('#inventory-app .item');
-let disabled=false
+    const items=[{
+        "name":"pasta",
+        "value":2,
+        
+    },{
+        "name":"green tea",
+        "value":0,
+    }];
 
-disableButtonIfNoInput();
-// disableMinusButtonWithZeroValue();
-addAbilityToAddItems();
-addAbilityToDeleteItems();
-addAbilityToIncreaseItems();
-addAbilityToDecreaseItems();
-render(items);
+    const inputItem=document.querySelector('#inventory-app .input-item');
+    const addButton=document.querySelector('#inventory-app .add-button');
+    const item=document.querySelector('#inventory-app .item');
 
-function render(items){
-    const html = items.map((items,index) =>{
-        return `
-        <li>
-            <button class="delete" data-index="${index}">X</button>
-            <span data-index="${index}">${items.name}</span>
-            <span class="counter">
-                <button class="minus" data-index="${index}" disabled="${items.value==0?"true":"false"}">-</button>
-                <span class="count">${items.value}</span>
-                <button class="plus" data-index="${index}">+</button>
-            </span>
-        </li>
-        `;
-    }).join('');
+    disableButtonIfNoInput();
+    addAbilityToAddItems();
+    addAbilityToDeleteItems();
+    addAbilityToIncreaseItems();
+    addAbilityToDecreaseItems();
+    render(items);
 
-    item.innerHTML= html;
-    addButton.disabled=!inputItem.value;
-};
-
-function addAbilityToAddItems(){
-    addButton.addEventListener('click', (e)=>{
-        const newItem={
-            name:inputItem.value,
-            value:0,
-            diableMinus:true
-        }
-        items.push(newItem);
-        inputItem.value='';
-        render(items);
-    })
-}
-
-function disableButtonIfNoInput(){
-    inputItem.addEventListener('input',()=>{
+    function render(items){
+        const html = items.map((items,index) =>{
+            return `
+            <li>
+                <button class="delete" data-index="${index}">X</button>
+                <span data-index="${index}">${items.name}</span>
+                <span class="counter">
+                    <button class="minus" data-index="${index}" ${items.value === 0 ? "disabled" : ""}>-</button>
+                    <span class="count">${items.value}</span>
+                    <button class="plus" data-index="${index}">+</button>
+                </span>
+            </li>
+            `;
+        }).join('');
+        item.innerHTML=html;
         addButton.disabled=!inputItem.value;
-    });
-}
-function addAbilityToIncreaseItems(){
-    item.addEventListener('click',(e)=>{
-        if(!e.target.classList.contains('plus')) {
-            return;
-          }
-        const index=e.target.dataset.index;
-        items[index].value=items[index].value+1;
-        //not working
-        if(items[index].value>0){
-                document.querySelector('.minus').disabled=false;
-        }
-        //
-        render(items);
-    })
-}
-function addAbilityToDecreaseItems(){
-    item.addEventListener('click',(e)=>{
-        if(!e.target.classList.contains('minus')) {
-            return;
-          }
-        const index=e.target.dataset.index;
-        items[index].value=items[index].value-1;
-        //not working
-        if(items[index].value==0){
-            document.querySelector('.minus').disabled=true;
-        }
-        //
-        render(items);
-    })
-}
-function addAbilityToDeleteItems(){
-    item.addEventListener('click',(e)=>{
-        if(!e.target.classList.contains('delete')) {
-            return;
-        }
-        const index=e.target.dataset.index;
-        items.splice(index,1);
-        render(items);
-    })
-}
-// function disableMinusButtonWithZeroValue(){
-//     items.forEach(element,index => {
-//         let ele=document.querySelector('.minus .${index}')
-//         if(element.value==0){
-//            ele.disabled=true;
-//         }else{
-//             ele.disabled=false;
-//         }
-//     });
-// }
+    };
 
+    function disableButtonIfNoInput(){
+        inputItem.addEventListener('input',()=>{
+            addButton.disabled=!inputItem.value;
+        });
+    }
 
-       
-    
+    function addAbilityToAddItems(){
+        addButton.addEventListener('click', (e)=>{
+            const newItem={
+                name:inputItem.value,
+                value:0,
+            }
+            items.push(newItem);
+            inputItem.value='';
+            render(items);
+        });
+    }
+
+    function addAbilityToIncreaseItems(){
+        item.addEventListener('click',(e)=>{
+            if(!e.target.classList.contains('plus')) {
+                return;
+            }
+            const index=e.target.dataset.index;
+            items[index].value=items[index].value+1;
+            render(items);
+        });
+    }
+
+    function addAbilityToDecreaseItems(){
+        item.addEventListener('click',(e)=>{
+            if(!e.target.classList.contains('minus')) {
+                return;
+            }
+            const index=e.target.dataset.index;
+            items[index].value=items[index].value-1;
+            render(items);
+        });
+    }
+
+    function addAbilityToDeleteItems(){
+        item.addEventListener('click',(e)=>{
+            if(!e.target.classList.contains('delete')) {
+                return;
+            }
+            const index=e.target.dataset.index;
+            items.splice(index,1);
+            render(items);
+        });
+    }
 
 })();
