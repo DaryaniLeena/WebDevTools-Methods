544217b9b297456899f85b839ef6e7265653fab9
DaryaniLeena <daryani.l@husky.neu.edu>
2020-11-01 21:21:36 -0800

    commit without login function

--- work/js-rest-spa/public/inventory.js
+++ work/js-rest-spa/public/inventory.js
@@ -1,218 +1,150 @@
 "use strict";
-
 (function iife() {
-    const errMsgs = {
-      'duplicate': 'That name already exists',
-      'network-error': 'There was a problem connecting to the network, try again',
-    };
-    const status = document.querySelector('.status');
-    const login= document.querySelector('#inventory-app .login-box')
-    const inputUserName= document.querySelector('#inventory-app .input-username');
-    const loginButton= document.querySelector('#inventory-app .login-button');
-    const inventory= document.querySelector('#inventory-app .inventory-box');
-    const itemList= document.querySelector('#inventory-app .item-class');
-    const logoutButton= document.querySelector('#inventory-app #logout-button');
 
-    disableLoginButtonIfNoInput();
-    // addAbilityToAddItems();
-    // addAbilityToDeleteItems();
-    // addAbilityToIncreaseItems();
-    // addAbilityToDecreaseItems();
-    // render(items);
+  const errMsgs = {
+    'duplicate': 'Status: That name already exists',
+    'network-error': 'Status: There was a problem connecting to the network, try again',
+    'bad-request': 'Status: Quantity cannnot be a string',
+    'missing-quantity': 'Status: Quantity cannot be empty',
+    'missing-item': 'Status: No such item Available',
+    'missing-name': 'Status: Name cannot be empty'
+  };
 
-    function renderPage(){
-        const html =
-            `
-            <div class="header-component">
-                <h3>INVENTORY</h3>
-            </div>
-            <div class="main-component">
-                <div>
-                    Add Item:
-                    <input class="input-item" placeholder="Enter item" >
-                    <input class="input-quantity" placeholder="Enter quantity">
-                    <button class="add-button">Add</button>
-                </div>
-                <ul class="item"></ul>
-            </div>
-            `;
-       
-        inventory.innerHTML=html;
-    };
+  const status = document.querySelector('.status');
+  const inputItem = document.querySelector('#inventory-app .input-item');
+  const inputQuantity = document.querySelector('#inventory-app .input-quantity');
+  const addButton = document.querySelector('#inventory-app .add-button');
+  const item = document.querySelector('#inventory-app .item');
+
+  loading();
+  disableButtonIfNoInput();
+
+  function updateStatus(message) {
+    status.innerText = message;
+  }
   
-    function updateStatus( message ) {
-        status.innerText = message;
+  function render(items) {
+    const html = Object.keys(items).map((item,index) => {
+      return `
+      <li>
+          <button class="delete" data-itemid="${item}">X</button>
+          <span data-itemid="${item}">${items[item].name}</span>
+          <span class="counter">
+            <span class="count" contenteditable="true">${items[item].value}</span>
+            <button class="update" data-itemid="${item}" data-index="${index}">Update</button>
+          </span>
+      </li>
+      `;
+    }).join('');
+    item.innerHTML = html;
+    addButton.disabled = !item.value;
+  };
+
+  function loading() {
+    const html = `
+      <li>
+         LOADING ...........
+      </li>
+      `;
+    item.innerHTML = html;
+  };
+
+  function convertError(response) {
+    if(response.ok) {
+      return response.json();
     }
-    // getUserList: function(chat) {
-    //   return `<ul class="users">` +
-    //     Object.values(chat.users).filter( user => user.active).map( user => `
-    //     <li>
-    //       <div class="user">
-    //         <span class="username">${user.username}</span>
-    //       </div>
-    //     </li>
-    //   `).join('') +
-    //   `</ul>`;
-    // },
-    
-    function render(items){
-        const html = Object.keys(items).map((item,index) => {
-            return `
-            <li>
-                <button class="delete" data-itemid="${item}">X</button>
-                <span data-itemid="${item}">${items[item].name}</span>
-                <span class="counter">
-                    <span class="count" contenteditable="true">${items[item].value}</span>
-                    <button class="update" data-itemid="${item}" data-index="${index}">Update</button>
-                </span>
-            </li>
-            `;
-        }).join('');
-        itemList.innerHTML=html;
-        document.querySelector('#inventory-app .add-button').disabled=!document.querySelector('#inventory-app .item').value;
-    };
+    return response.json()
+    .then( err => Promise.reject(err) );
+  }
 
-    function disableLoginButtonIfNoInput(){
-        inputUserName.addEventListener('input',()=>{
-            loginButton.disabled=!inputUserName.value;
-        });
-    }
-
-    loginButton.addEventListener('click', () => {
-        const name = inputUserName.value;
-        if(name) {
-          fetch(`/session/${name}`, {
-            method: 'POST',
-          })
-          .catch( () => Promise.reject( { error: 'network-error' }) )
-          .then( convertError)
-          .then( items => {
-            inputUserName.value = '';
-            login.style.display = 'none';
-            inventory.style.display = 'block';
-            itemList.style.display = 'block';
-            logoutButton.style.display = 'block';
-            renderPage();
-            render(items);
-            disableButtonIfNoInput();
-            addAbilityToAddItems();
-            addAbilityToDeleteItems();
-            enablelogout();
-            addAbilityToUpdateCount();
-            updateStatus(`logged in user is ${name}`);
-          })
-          .catch( err => {
-            updateStatus(errMsgs[err.error] || err.error);
-          });
-        }
-      });
-
-      function convertError(response) {
-        if(response.ok) {
-          return response.json();
-        }
-        return response.json()
-        .then( err => Promise.reject(err) );
-      }
-    
-
-    function disableButtonIfNoInput(){
-      document.querySelector('#inventory-app .input-item').addEventListener('input',()=>{
-        document.querySelector('#inventory-app .add-button').disabled=!document.querySelector('#inventory-app .input-item').value;
-        });
-    }
-
-    function addAbilityToAddItems(){
-      document.querySelector('#inventory-app .add-button').addEventListener('click', () => {
-      const name = document.querySelector('#inventory-app .input-item').value;
-      let quantity=document.querySelector('#inventory-app .input-quantity').value;
-      if(quantity === ""){
-        quantity=0;
-      }
-      if(name) {
-        fetch(`/items/${name}/${quantity}`, {
-          method: 'POST',
-        })
-        .catch( () => Promise.reject( { error: 'network-error' }) )
-        .then( convertError)
-        .then( items => {
-          document.querySelector('#inventory-app .input-item').value = '';
-          document.querySelector('#inventory-app .input-quantity').value = '';
-          render(items);
-          updateStatus('');
-        })
-        .catch( err => {
-          updateStatus(errMsgs[err.error] || err.error);
-          document.querySelector('#inventory-app .input-item').value = '';
-          document.querySelector('#inventory-app .input-quantity').value = '';
-        });
-      }
+  function disableButtonIfNoInput() {
+    inputItem.addEventListener('input', () => {
+      addButton.disabled =! inputItem.value;
     });
-    }
+  }
 
-    function addAbilityToDeleteItems(){
-      itemList.addEventListener('click',(e)=>{
-        if(e.target.classList.contains('delete') ) {
-            const index = e.target.dataset.itemid;
-            fetch(`/items/${index}`, {
-              method: 'DELETE',
-            })
-            .catch( () => Promise.reject( { error: 'network-error' }) )
-            .then( convertError )
-            .then( items => {
-              render(items);
-              updateStatus('');
-            })
-            .catch( err => {
-              updateStatus(errMsgs[err.error] || err.error);
-            });
-            
-        }
-      });
+  addButton.addEventListener('click', () => {
+    const name = inputItem.value;
+    let quantity = inputQuantity.value;
+    if(quantity === ""){
+      quantity = 0;
     }
-
-    function enablelogout(){
-      logoutButton.addEventListener('click', () => {
-        fetch(`/session`, {
-          method: 'POST',
-        })
-        .catch( () => Promise.reject( { error: 'network-error' }) )
-        .then(convertError)
-        .then( ()=> {
-        login.style.display = 'block';
-        inventory.style.display = 'none';
-        itemList.style.display = 'none';
-        logoutButton.style.display = 'none';
+    if(name) {
+      fetch(`/items/${name}/${quantity}`, {
+        method: 'POST',
+      })
+      .catch( () => Promise.reject( { error: 'network-error' }) )
+      .then( convertError)
+      .then( items => {
+        resetAddItemField();
+        render(items);
         updateStatus('');
-        })
-        .catch( err => {
-          updateStatus(errMsgs[err.error] || err.error);
-        });
+      })
+      .catch( err => {
+        resetAddItemField();
+        updateStatus(errMsgs[err.error] || err.error);
       });
     }
-
-    function addAbilityToUpdateCount(){
-      itemList.addEventListener('click',(e)=>{
-        if(e.target.classList.contains('update') ) {
-          const index=e.target.dataset.index;
-          const itemid=e.target.dataset.itemid;
-          const quantity=(document.querySelectorAll('#inventory-app .count')[index]).innerText;
-            fetch(`/items/${itemid}/${quantity}`, {
-              method: 'PATCH',
-            })
-            .catch( () => Promise.reject( { error: 'network-error' }) )
-            .then( convertError )
-            .then( items => {
-              render(items);
-              updateStatus('');
-            })
-            .catch( err => {
-              updateStatus(errMsgs[err.error] || err.error);
-            });
-            
-        }
-      });
+  });
+  
+  item.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete') ) {
+      const itemid = e.target.dataset.itemid;
+      fetch(`/items/${itemid}`, {
+        method: 'DELETE',
+      })
+      .catch( () => Promise.reject( { error: 'network-error' }) )
+      .then( convertError )
+      .then( items => {
+        render(items); 
+        updateStatus('');
+      })
+      .catch( err => {
+        updateStatus(errMsgs[err.error] || err.error);
+      }); 
     }
+  });
+  
+  item.addEventListener('click', (e) => {
+    if(e.target.classList.contains('update') ) {
+      const index = e.target.dataset.index;
+      const itemid = e.target.dataset.itemid;
+      let quantity = (document.querySelectorAll('#inventory-app .count')[index]).innerText;
+      if(quantity === ""){
+        quantity = 0;
+      }
+      fetch(`/items/${itemid}/${quantity}`, {
+        method: 'PATCH',
+      })
+      .catch( () => Promise.reject({ error: 'network-error' }) )
+      .then( convertError )
+      .then( items => {
+        render(items);
+        updateStatus('');
+      })
+      .catch( err => {
+        updateStatus(errMsgs[err.error] || err.error);
+      }); 
+    }
+  });
 
+  fetch('/items/', {
+  method: 'GET',
+  })
+  .catch( () => Promise.reject( { error: 'network-error' }) )
+  .then( convertError )
+  .then( items => {
+    render(items);
+    updateStatus('');
+  })
+  .catch( err => {
+    updateStatus(errMsgs[err.error] || err.error);
+  });
+  
+  function resetAddItemField(){
+    inputItem.value = '';
+    inputQuantity.value = '';
+    addButton.disabled = true;
+  }
 
 })();
--- work/js-rest-spa/server.js
+++ work/js-rest-spa/server.js
@@ -1,80 +1,33 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const cookieParser = require('cookie-parser');
+
 app.use(express.static('./public'));
-app.use(cookieParser());
 
-app.use(express.urlencoded({ extended: false }));
-const users = {
-    "11": {
-      username: "Amit",
-      active: false,
-      uid: 11,
-    },
-    "43": {
-      username: "Bao",
-      active: false,
-      uid: 43,
-    },
+const counter = () => {
+  let count = 2; // so that itemid starts from 3, i & 2 is already given for first two items
+  return () => {
+      count += 1;
+      return count;
   };
+};
 
-// const items=[{
-//   "name":"pasta",
-//   "value":2,
-  
-// },{
-//   "name":"green tea",
-//   "value":0,
-// }];
+nextID = counter();
 const items={
   "1":{
     "name":"pasta",
     "value":2,
     "itemid":1
-},
-"2":{
-  "name":"green tea",
-  "value":0,
-  "itemId":2
-}};
-
-app.get('/', (req, res) => {
-  const uid = req.cookies && req.cookies.uid;
-  if(uid && users[uid]) {
-    res.json(tems);
+  },
+  "2":{
+    "name":"green tea",
+    "value":0,
+    "itemId":2
   }
-  console.log(users);
-});
+};
 
-app.post('/session/:name', express.json(), (req, res) => {
-  const username  = req.params.name;
-  console.log(username);
-  if(!username || username.includes("dog") || username.includes(" ")){
-    res.status(400).json({ error: 'bad-login' });
-    return;
-  }
-  const record = Object.values(users).find(user => user.username === username);
-  const oldId = record && record.uid;
-  const id = oldId || Math.floor(Math.random() * 10000);
-  
-  users[id] = { username, active: true, uid: id };
-   console.log(users);
-  res.cookie('uid', id);
-  res.json(items);
-});
-
-// app.get('/people/:name', (req, res) => {
-//     const name = req.params.name;
-//     if(people[name]) {
-//       res.json(people[name]);
-//     } else {
-//       res.status(404).json({ error: `Unknown user: ${name}`});
-//     }
-//   });
-  
 app.get('/items/', (req, res) => {
-  res.json(tems);
+  res.json(items);
 });
   
 app.post('/items/:name/:quantity', express.json(), (req, res) => {
@@ -89,51 +42,39 @@ app.post('/items/:name/:quantity', express.json(), (req, res) => {
     res.status(409).json({ error: 'duplicate' });
     return;
   }
-  const x=(Math.floor(Math.random() * 98) + 3); // random number between 3 and 100 (2 and 1 is already given for first two)
+  const itemid=nextID();
   const newItem={
     name: name,
-    value: quantity,
-    itemid: x 
+    value: parseInt(quantity),
+    itemid: itemid 
   }
-  items[x] = newItem;
-  // items.push(newItem);
-  console.log(items);
+  items[itemid] = newItem;
   res.json(items);
 });
   
-app.delete('/items/:index', (req, res) => {
-  const index = req.params.index;
-  console.log(index);
-  console.log(items);
-  if(!items[index]) {
-    res.status(400).json({ error: 'missing-name' });
+app.delete('/items/:itemid', (req, res) => {
+  const itemid = req.params.itemid;
+  if(!items[itemid]) {
+    res.status(400).json({ error: 'missing-item' });
     return;
   }
-  // items.splice(index,1);
-  delete items[index];
-  console.log(items);
+  delete items[itemid];
   res.json(items);
-});
-
-app.post('/session', (req, res) => {
-  const uid = req.cookies.uid;
-  if(users[uid]) {
-    users[uid].active = false;
-  }
-  
-  res.clearCookie('uid');
-  res.send({err: 0, redirectUrl: "/"});
 });
 
 app.patch('/items/:itemid/:quantity', express.json(), (req, res) => {
-  const index = req.params.itemid;
+  const itemid = req.params.itemid;
   const quantity = req.params.quantity;
-  if(!items[index]) {
-    res.status(400).json({ error: 'missing-name' });
+  
+  if(isNaN(quantity)){
+    res.status(400).json({ error: 'bad-request' });
+    return;
+  }
+  if(!items[itemid]) {
+    res.status(400).json({ error: 'missing-item' });
     return;
   } 
-  items[index].value=parseInt(quantity);
-  console.log(items);
+  items[itemid].value=parseInt(quantity);
   res.json(items);
 });
 
